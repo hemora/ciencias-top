e4faeb4bebeecd9e1408a585b8e04c61c20a2ad2
Rulox710 <rnunovaldes@.ciencias.unam.mx>
2022-11-09 01:34:48 -0600

    Implementando cosas referentes a los Usuarios

---
+++ cienciastop/src/main/java/com/fciencias/cienciastop/models/controllers/UsuarioRestController.java
@@ -0,0 +1,55 @@
+package com.fciencias.cienciastop.models.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fciencias.cienciastop.models.entity.Producto;
+import com.fciencias.cienciastop.models.entity.Usuario;
+import com.fciencias.cienciastop.models.service.IUsuarioService;
+
+@CrossOrigin(origins= {"http://localhost:4200"})
+@RestController
+@RequestMapping("/api")//ciencias-top?
+/**
+ * Clase controlador de usuario (aka. Administrador)
+ * 
+ */
+public class UsuarioRestController {
+
+	@Autowired
+	private IUsuarioService usuarioService;
+	
+	@GetMapping("/usuarios")
+	public List<Usuario> verUsuarios() {
+		return usuarioService.verUsuarios();
+	}
+	
+	@GetMapping("/usuarios/{noCT}")
+	public Usuario buscarUsuario(@PathVariable int noCT) {
+		return usuarioService.buscarUsuarioPorNoCT(noCT);
+	}
+	
+	@PostMapping("/usuarios")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Usuario agregarUsuario(@RequestBody Usuario usuario) {
+		return usuarioService.guardar(usuario);
+	}
+	
+	@PutMapping("/usuarios/{noCT}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminatUsuario(@PathVariable int noCT) {
+		usuarioService.borrar(noCT);
+	}
+}
---
+++ cienciastop/src/main/java/com/fciencias/cienciastop/models/dao/IUsuarioDao.java
@@ -0,0 +1,36 @@
+package com.fciencias.cienciastop.models.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.fciencias.cienciastop.models.entity.Usuario;
+
+@Repository
+public interface IUsuarioDao extends CrudRepository<Usuario, Integer> {
+	
+	@Query(value= "SELECT * FROM usuarios WHERE status = :status", nativeQuery = true)
+	List<Usuario> encontrarPorStatus(@Param("status") Integer status);
+	
+	@Query(value= "SELECT * FROM usuarios WHERE noCT = :noCT AND status = 1", nativeQuery = true)
+	Usuario encontrarPorNoCT(@Param("noCT") Integer noCT);
+	
+	@Query(value= "SELECT * FROM usuarios WHERE correo = :correo", nativeQuery = true)
+	Usuario encontrarPorCorreo(@Param("correo") String correo);
+	
+	@Modifying
+	@Transactional
+	@Query(value ="UPDATE usuarios SET status = 1 WHERE noCT = :noCT", nativeQuery = true)
+	Integer activar(@Param("noCT") Integer noCT);
+	
+	@Modifying
+	@Transactional
+	@Query(value ="UPDATE usuarios SET status = 0 WHERE noCT = :noCT", nativeQuery = true)
+	Integer desactivar(@Param("noCT") Integer noCT);
+}
