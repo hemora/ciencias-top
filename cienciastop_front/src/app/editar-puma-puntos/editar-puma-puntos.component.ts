a66ffbae47850d4632ab5a73217f4fdf74b8ae6c
hemora <hmora@ciencias.unam.mx>
2022-11-11 12:42:36 -0600

    primer del frontend para sumar/restar pumapuntos

---
+++ cienciastop_front/src/app/editar-puma-puntos/editar-puma-puntos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-puma-puntos',
+  templateUrl: './editar-puma-puntos.component.html',
+  styleUrls: ['./editar-puma-puntos.component.css']
+})
+export class EditarPumaPuntosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
