0fe1c9e0ef7c0a8b7c30ae62adc0e5a05aa4c8e9
Leo2711 <leonel2711@ciencias.unam.mx>
2022-11-14 18:59:05 -0600

    agregarUsuario con excepciones

--- cienciastop/src/main/java/com/fciencias/cienciastop/models/controllers/UsuarioRestController.java
+++ cienciastop/src/main/java/com/fciencias/cienciastop/models/controllers/UsuarioRestController.java
@@ -63,8 +63,25 @@ public class UsuarioRestController {
 	
 	@PostMapping("/usuarios")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Usuario agregarUsuario(@RequestBody Usuario usuario) {
-		return usuarioService.guardar(usuario);
+	public ResponseEntity<?> agregarUsuario(@RequestBody Usuario usuario) {
+		Usuario usuarioNuevo = null;
+		Map<String,Object> response = new HashMap<>();
+		try {
+			usuarioNuevo = usuarioService.guardar(usuario);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar el insert en la base de datos.")
+			response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String,Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+			// TODO: handle exception
+		}
+		if(usuarioNuevo == null){
+			response.put("mensaje", "El usuario se ha reactivado con éxito. :D")
+			response.put("usuario", usuarioNuevo); //??
+			return new ResponseEntity<Map<String,Object>>(response,HttpStatus.CREATED);
+		}
+		response.put("mensaje", "El usuario se ha creado con éxito. :D")
+		response.put("usuario", usuarioNuevo);
+		return new ResponseEntity<Map<String,Object>>(response,HttpStatus.CREATED);
 	}
 	
 	@PutMapping("/usuarios/{noCT}")
