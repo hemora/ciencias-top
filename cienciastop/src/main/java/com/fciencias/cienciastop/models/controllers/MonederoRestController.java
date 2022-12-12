ea0665d38ad3af871976dc00233c7aa973c43045
hemora <hmora@ciencias.unam.mx>
2022-12-11 22:40:13 -0600

    modificados métodos pumapuntos para accesibilidad

--- cienciastop/src/main/java/com/fciencias/cienciastop/models/controllers/MonederoRestController.java
+++ cienciastop/src/main/java/com/fciencias/cienciastop/models/controllers/MonederoRestController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -102,6 +103,7 @@ public class MonederoRestController {
     }
 
     @PutMapping("/monederos/{id}/{pp}")
+	@PreAuthorize("hasRole('Administrador')")
     public ResponseEntity<?> sumarRestarPumaPuntos(@PathVariable Long id, @PathVariable Double pp) {
 
         String pattern = "yyyy-MM";
--- cienciastop_front/src/app/editar-puma-puntos/monedero.service.ts
+++ cienciastop_front/src/app/editar-puma-puntos/monedero.service.ts
@@ -3,17 +3,25 @@ import { catchError, Observable, throwError } from 'rxjs';
 import  { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Monedero } from './monedero';
 import Swal from 'sweetalert2';
+import { UserAuthService } from '../util/user-auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MonederoService {
 
-  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private userAuthService: UserAuthService) { }
 
   private monederoApi: string = 'http://localhost:8080/api/monederos'
+
+  private authHeader = {
+    headers: new HttpHeaders()
+      .set('Authorization',  `Bearer ${this.userAuthService.getToken()}`)
+  }
+  
+  private httpHeaders = new HttpHeaders()
+      .set('Authorization',  `Bearer ${this.userAuthService.getToken()}`)
+      .set('Content-Type',  'application/json')
 
   getMonedero(id: number, periodo: string) {
     //var monedero = new Monedero();
@@ -21,7 +29,7 @@ export class MonederoService {
     //monedero.ownerId = 0;
     //monedero.status = "";
     //monedero.pumaPuntos = 0;
-    return this.http.get<any>(this.monederoApi + '/' + id + '/' + periodo).pipe(
+    return this.http.get<any>(this.monederoApi + '/' + id + '/' + periodo, this.authHeader).pipe(
       catchError(e => {
         Swal.fire('Error al obtener el monedero', e.error.mensaje, 'error');
         return throwError( () => e);
@@ -31,9 +39,9 @@ export class MonederoService {
   }
 
   sumarRestarPumaPuntos(id: number, pp: number) {
-    return this.http.put<any>(this.monederoApi + '/' + id + '/' + pp, {headers: this.httpHeaders}).pipe(
+    return this.http.put<any>(this.monederoApi + '/' + id + '/' + pp, null, {headers: this.httpHeaders}).pipe(
       catchError( e => {
-        Swal.fire('Error al sumar/restar puma puntos', e.error.mensaje, 'error');
+        Swal.fire('Error al sumar/restar puma puntos', e.error, 'error');
         return throwError( () => e);
       })
     )
