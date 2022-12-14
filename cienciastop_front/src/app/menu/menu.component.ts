4681b54e93348eba16d7f82b32996fedcd122c3d
monica <monmm@ciencias.unam.mx>
2022-12-13 20:03:29 -0600

    Agregamos el nombre de usr en el menu

--- cienciastop_front/src/app/menu/menu.component.ts
+++ cienciastop_front/src/app/menu/menu.component.ts
@@ -12,13 +12,21 @@ import { Router } from '@angular/router';
 export class MenuComponent implements OnInit {
 
   noCT : number;
+  nombre : string;
+  map_data = new Map();
   
   constructor(public authService: UserAuthService, private usuarioService: UsuarioService, private router: Router) {
   }
   
   
   ngOnInit(): void {        
-    this.noCT = this.authService.getNoCta();    
+    this.noCT = this.authService.getNoCta();
+    this.usuarioService.buscarUsuario(this.noCT).subscribe(
+      response => {
+        this.map_data = new Map(Object.entries(response));                         
+        console.log(this.map_data);
+        this.nombre = this.map_data.get("nombre") + " " + this.map_data.get("apellidos");
+  });            
     console.log(this.noCT);
   }
 
